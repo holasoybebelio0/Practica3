475d59cf97db0619bea186a2edc67c5e7b1c0669
holasoybebelio0 <ervacero0908@gmail.com>
2025-11-30 12:09:00 +0100

    prueba para aran

--- src/aplicacio/App.java
+++ src/aplicacio/App.java
@@ -1,6 +1,6 @@
 package aplicacio;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, mundo!");
+        System.out.println("Projecte en java URV programacio");
     }
 }
