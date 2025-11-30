72398adc79133492f38755e8ab224ddf6b5dd385
holasoybebelio0 <ervacero0908@gmail.com>
2025-11-30 19:51:19 +0100

    inscripicones

---
+++ src/dades/inscripcions.java
@@ -0,0 +1,10 @@
+package dades;
+import java.io.Serializable;
+
+public class inscripcions implements Serializable { 
+    private String nom;
+    private String nomActivitat;
+    private dataInici;
+
+    
+}
