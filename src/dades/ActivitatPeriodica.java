fcb6f9998b1890965d14863115de265686a13e00
guacacamole <paucal06@gmail.com>
2025-11-30 21:12:30 +0100

    ActivitatPeriodica

--- src/dades/ActivitatPeriodica.java
+++ src/dades/ActivitatPeriodica.java
@@ -1,5 +1,115 @@
 package dades;
 
-public class ActivitatPeriodica {
+public class ActivitatPeriodica extends Activitat{
+    private String diaSetmana; 
+    private String horari; 
+    private String dataInici; 
+    private int nSetmanes;
+    private int places;
+    private double preu; 
+    private String nomCentre;
+    private String ciutat;
+
+    public ActivitatPeriodica (String nom, String[] colectius, String dataIniciInscripcio, String dataFinalInscripcio, String diaSetmana, String horari, String dataInici, int nSetmanes, int places, double preu, String nomCentre, String ciutat) {
+        super(nom, colectius, dataIniciInscripcio, dataFinalInscripcio);
+        this.diaSetmana = diaSetmana;
+        this.horari = horari;
+        this.dataInici = dataInici;
+        this.nSetmanes = nSetmanes;
+        this.places = places;
+        this.preu = preu; 
+        this.nomCentre = nomCentre;
+        this.ciutat = ciutat; 
+    }
+
+    //Getters i setters
+    public String getDiaSetmana() {
+        return diaSetmana;
+    }
+
+    public void setDiaSetmana(String diaSetmana) {
+        this.diaSetmana = diaSetmana;
+    }
+
+    public String getHorari() {
+        return horari;
+    }
+
+    public void setHorari(String horari) {
+        this.horari = horari;
+    }
+
+    public String getDataInici() {
+        return dataInici;
+    }
+
+    public void setDataInici(String dataInici) {
+        this.dataInici = dataInici;
+    }
+
+    public int getnSetmanes() {
+        return nSetmanes;
+    }
+
+    public void setnSetmanes(int nSetmanes) {
+        this.nSetmanes = nSetmanes;
+    }
+
+    public int getPlaces() {
+        return places;
+    }
+
+    public void setPlaces(int places) {
+        this.places = places;
+    }
+
+    public double getPreu() {
+        return preu;
+    }
+
+    public void setPreu(double preu) {
+        this.preu = preu;
+    }
+
+    public String getNomCentre() {
+        return nomCentre;
+    }
+
+    public void setNomCentre(String nomCentre) {
+        this.nomCentre = nomCentre;
+    }
+
+    public String getCiutat() {
+        return ciutat;
+    }
+
+    public void setCiutat(String ciutat) {
+        this.ciutat = ciutat;
+    }
+
+    //To string
+    @Override
+    public String toString() {
+        return "ActivitatPeriodica [diaSetmana=" + diaSetmana + ", horari=" + horari + ", dataInici=" + dataInici
+                + ", nSetmanes=" + nSetmanes + ", places=" + places + ", preu=" + preu + ", nomCentre=" + nomCentre
+                + ", ciutat=" + ciutat + "]";
+    }
+    
+    @Override
+    public Activitat copia() {
+        return new ActivitatPeriodica(
+            this.nom,
+            this.colectius,
+            this.dataIniciInscripcio,
+            this.dataFinalInscripcio,
+            this.diaSetmana,
+            this.horari,
+            this.dataInici,
+            this.nSetmanes,
+            this.places,
+            this.preu,
+            this.nomCentre,
+            this.ciutat
+        );
 
 }
