22f5ccdb38e56408bd65b6b7c9ae7f1a8e2ae540
holasoybebelio0 <145043503+holasoybebelio0@users.noreply.github.com>
2025-12-01 10:08:05 +0100

    inscripcions y llistainscripcions

--- src/dades/LlistaInscripcions.java
+++ src/dades/LlistaInscripcions.java
@@ -1,5 +1,40 @@
 package dades;
 
 public class LlistaInscripcions {
-    
+    private inscripcions[] LlistaIncripcions;
+    private int numInscripcions;
+    //CONSTUCTOR
+    public LlistaInscripcions(int capacitat){
+        LlistaIncripcions = new inscripcions[capacitat];
+        numInscripcions = 0;
+    }
+
+    //GETTERS I SETTERS
+    public inscripcions getInscripcio(int index) {
+        if (index >= 0 && index < numInscripcions) {
+            return LlistaIncripcions[index];
+        } else {
+            System.out.println("Índex fora de rang.");
+            return null;
+        }
+    }   
+    public int getNumInscripcions() {
+        return numInscripcions;
+    }
+    public void afegirInscripcio(inscripcions inscripcio) {
+        if (numInscripcions < LlistaIncripcions.length) {
+            LlistaIncripcions[numInscripcions] = inscripcio;
+            numInscripcions++;
+        } else {
+            System.out.println("No es poden afegir més inscripcions, capacitat plena.");
+        }
+    }
+    public LlistaInscripcions copia() {
+        LlistaInscripcions llistaCopiada = new LlistaInscripcions(LlistaIncripcions.length);
+        for (int i = 0; i < numInscripcions; i++) {
+            llistaCopiada.afegirInscripcio(LlistaIncripcions[i]);
+        }
+        return llistaCopiada;
+    }
+
 }
--- src/dades/inscripcions.java
+++ src/dades/inscripcions.java
@@ -1,7 +1,33 @@
 package dades;
-import java.io.Serializable;
 
-public class inscripcions implements Serializable { 
+
+public class inscripcions { 
+    private int numInscripcions;
+    private int numPlaces;
+
+    /**CONSTRUCTOR */
+    public inscripcions (int numInscripcions, int numPlaces){
+        this.numInscripcions = numInscripcions;
+        this.numPlaces = numPlaces;
+    }
+
+    //GETTERS I SETTERS
+    public int getNumInscripcions() {
+        return numInscripcions;
+    }   
+
+    public int getNumPlaces() {
+        return numPlaces;
+    }
+
+    public void setNumPlaces(int numPlaces) {
+        this.numPlaces = numPlaces;
+    }
+
+    public void setNumInscripcions(int insc){
+        this.numInscripcions = insc;
+
+    }
     
     
 }
