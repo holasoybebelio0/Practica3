3f15a0bc2f4f273cd53c5c57de78b737bb1ed063
holasoybebelio0 <145043503+holasoybebelio0@users.noreply.github.com>
2025-12-01 10:53:37 +0100

    mains

--- src/aplicacio/ValidacioInscripcions.java
+++ src/aplicacio/ValidacioInscripcions.java
@@ -2,7 +2,7 @@ package aplicacio;
 
 import dades.*;
 
-public class App {
+public class ValidacioInscripcions {
     public static void main(String[] args) throws Exception {
 
         System.out.println("Projecte en java URV programacio");
